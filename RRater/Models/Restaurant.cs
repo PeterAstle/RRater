d1d8b0be31abd3ad0ba9acd004cea135e2b61873
PeterAstle <79322373+PeterAstle@users.noreply.github.com>
2021-05-03 10:35:09 -0400

    db initial setup

---
+++ RRater/Models/Restaurant.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace RRater.Models
+{
+    public class Restaurant
+    {
+        public int RestaurantId { get; set; }
+
+        public string Name { get; set; }
+
+        public string Address { get; set; }
+
+        public int Rating { get; set; }
+                
+    }
+
+    public class RestaurantDbContext : DbContext
+    {
+        public DbSet<Restaurant> Restaurants { get; set; }
+    }
+}
