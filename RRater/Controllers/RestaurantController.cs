4234e577bba5f2889da90404c570b94f39f2ddd3
PeterAstle <79322373+PeterAstle@users.noreply.github.com>
2021-05-03 11:15:15 -0400

    setup restaurant index view

--- RRater/Controllers/RestaurantController.cs
+++ RRater/Controllers/RestaurantController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using RRater.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,10 +9,12 @@ namespace RRater.Controllers
 {
     public class RestaurantController : Controller
     {
+        private RestaurantDbContext _context = new RestaurantDbContext();
+
         // GET: Restaurant/Index
         public ActionResult Index()
         {
-            return View();
+            return View(_context.Restaurants.ToList()); ;
         }
     }
 }
