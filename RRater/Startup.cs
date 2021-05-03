265ef2cd30cf51de2df6ef43db6fe3e519d8f510
PeterAstle <79322373+PeterAstle@users.noreply.github.com>
2021-05-03 10:15:06 -0400

    Add project files.

---
+++ RRater/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(RRater.Startup))]
+namespace RRater
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
